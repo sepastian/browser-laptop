da3e150f1f35c3941b98a3bc936567ccc049a034
Pete Miller <miller.pete+commit@gmail.com>
2017-11-26 22:06:10 -0800

    perf: when choosing 'Toggle react profiling', also record mergeProps 'wasted' time, total time and number of invocations
    This option counts:
    - all time that is spent in all component mergeProps functions that results in no properties actually changing (and therefore no render), every 1 second. This is an important metric to improve since this is blocking the renderer window UI thread, and the time is likely spent re-computing functions even though the state input to those functions has not changed.
    - total time for each Component-type spent in mergeProps, output every 10 seconds
    - number of invocations for each Component-type mergeProps, output every 10 seconds

--- app/renderer/components/reduxComponent.js
+++ app/renderer/components/reduxComponent.js
@@ -1,8 +1,10 @@
+const ipc = require('electron').ipcRenderer
 const appStore = require('../../../js/stores/appStoreRenderer')
 const ImmutableComponent = require('./immutableComponent')
 const React = require('react')
 const windowStore = require('../../../js/stores/windowStore')
 const {isList, isSameHashCode} = require('../../common/state/immutableUtil')
+const messages = require('../../../js/constants/messages')
 
 const mergePropsImpl = (stateProps, ownProps) => {
   return Object.assign({}, stateProps, ownProps)
@@ -39,6 +41,55 @@ const didPropsChange = function (oldProps, newProps) {
   return false
 }
 
+let isProfiling = false
+let perfRunningIntervalWastedTime = null
+let perfRunningIntervalMergePropsHistory = null
+let mergePropsWasteMs = 0
+let mergePropsComponentHistory = { }
+ipc.on(messages.DEBUG_REACT_PROFILE, (e, args) => {
+  if (!isProfiling) {
+    isProfiling = true
+    let totalMergePropsWaste = 0
+
+    const logMergePropsWaste = function () {
+      const timeWasted = mergePropsWasteMs
+      if (timeWasted) {
+        mergePropsWasteMs = 0
+        totalMergePropsWaste += timeWasted
+        console.log(`wasted ${timeWasted}ms in the last 1 second performing mergeProps that did not change, now wasted a total of ${totalMergePropsWaste}ms`)
+      }
+    }
+    const logMergePropsHistory = function () {
+      let toLog = mergePropsComponentHistory
+      mergePropsComponentHistory = { }
+      // sort by time taken
+      let toLogArray = []
+      for (const componentName in toLog) {
+        const { totalTime, invocations } = toLog[componentName]
+        toLogArray.push({ componentName, totalTime, invocations })
+      }
+      toLogArray.sort((a, b) => b.totalTime - a.totalTime)
+      toLog = { }
+      for (const {componentName, totalTime, invocations} of toLogArray) {
+        toLog[componentName] = { totalTime, invocations }
+      }
+      const totalBlockingTime = toLogArray.reduce((total, current) => total + current.totalTime, 0)
+      console.log(`MergeProps history over the last 10 seconds (total UI blocking time = ${totalBlockingTime}ms): `)
+      console.table(toLog)
+    }
+    perfRunningIntervalWastedTime = setInterval(logMergePropsWaste, 1000)
+    perfRunningIntervalMergePropsHistory = setInterval(logMergePropsHistory, 10000)
+  } else {
+    window.clearInterval(perfRunningIntervalWastedTime)
+    window.clearInterval(perfRunningIntervalMergePropsHistory)
+    isProfiling = false
+    mergePropsWasteMs = null
+    // TODO: show count of Component's running mergeProps to re-evaluate props from state,
+    // in order to track how many components are subscribing to state,
+    // as a performance metric to strive to reduce
+  }
+})
+
 class ReduxComponent extends ImmutableComponent {
   constructor (componentType, mergeStateToProps, props) {
     super(props)
@@ -51,9 +102,27 @@ class ReduxComponent extends ImmutableComponent {
 
   checkForUpdates () {
     if (!this.dontCheck) {
+      const t0 = isProfiling && window.performance.now()
       const newState = this.buildProps(this.props)
+      const t1 = isProfiling && window.performance.now()
       if (didPropsChange(this.state, newState)) {
         this.setState(newState)
+      } else if (isProfiling) {
+        // log time used up in mergeProps where nothing changed
+        const timeTaken = t1 - t0
+        mergePropsWasteMs += timeTaken
+      }
+      // log how much total time was taken, whether something changed or not,
+      // so we can asses which Components mergeProps functions are taking the most time
+      if (isProfiling) {
+        const componentName = this.componentType.name
+        let componentMergePropsHistory = mergePropsComponentHistory[componentName]
+        if (!componentMergePropsHistory) {
+          componentMergePropsHistory = { totalTime: 0, invocations: 0 }
+          mergePropsComponentHistory[componentName] = componentMergePropsHistory
+        }
+        componentMergePropsHistory.invocations++
+        componentMergePropsHistory.totalTime += (t1 - t0)
       }
     }
   }
